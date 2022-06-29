fd9ef0b8c9bcbde003c1910d132265e98d21795b
Hassane Bendaoud <40140987+realHassaneBen@users.noreply.github.com>
2022-06-29 16:21:34 +0100

    Update session.js

--- src/config/session.js
+++ src/config/session.js
@@ -1,7 +1,6 @@
 export const sessionConfig = {
     secret: process.env.SESSION_SECRET,
     name: process.env.SESSION_NAME,
-    keys: ["key1", "key2"], // for multiple keys use an array of keys for encryption and decryption of the session cookie data
     resave: false, // we support the touch method so per the express-session docs this should be set to false
     proxy: true, // if you do SSL outside of node.
     saveUninitialized: false, // create a session even if there is no cookie
