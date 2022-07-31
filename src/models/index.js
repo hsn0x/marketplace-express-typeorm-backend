6a8713c8b97236f8897d989cb091c3faed04fafe
Hassane Bendaoud <40140987+realHassaneBen@users.noreply.github.com>
2022-07-31 17:17:57 +0100

    adding typescript

--- src/models/index.js
+++ src/models/index.js
@@ -1,21 +1,23 @@
-import Market from "./Market.js";
-import Product from "./Product.js";
-import Student from "./Student.js";
-import User from "./User.js";
-import Category from "./Category.js";
-import Post from "./Post.js";
-import Image from "./Image.js";
-import Avatar from "./Avatar.js";
-import Video from "./Video.js";
-import Comment from "./Comment.js";
-import Review from "./Review.js";
-import Role from "./Role.js";
-import Permission from "./Permission.js";
-import Resource from "./Resource.js";
-import Category_Categoryable from "./Category_Categoryable.js";
-import Like from "./Like.js";
-import Vote from "./Vote.js";
-import Favorite from "./Favorite.js";
+import Market from "./Market.js"
+import Product from "./Product.js"
+import Student from "./Student.js"
+import User from "./User.js"
+import Category from "./Category.js"
+import Post from "./Post.js"
+
+import Image from "./Image.js"
+import Avatar from "./Avatar.ts"
+
+import Video from "./Video.js"
+import Comment from "./Comment.js"
+import Review from "./Review.js"
+import Role from "./Role.js"
+import Permission from "./Permission.js"
+import Resource from "./Resource.js"
+import Category_Categoryable from "./Category_Categoryable.js"
+import Like from "./Like.js"
+import Vote from "./Vote.js"
+import Favorite from "./Favorite.js"
 
 export {
     Market,
@@ -36,4 +38,4 @@ export {
     Like,
     Vote,
     Favorite,
-};
+}
